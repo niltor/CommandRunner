f15917a1c54fa9dc51a541a1a393a340c0a1bab8
NilTor <zpty@outlook.com>
2017-05-12 21:35:14 +0800

    start package

---
+++ MSDev.Tools.CommandRunner/JsonFileHelper.cs
@@ -0,0 +1,112 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using CommandRunner.Models;
+using Newtonsoft.Json;
+
+namespace MSDev.Tools.CommandRunner
+{
+  /// <summary>
+  /// 每个文件代表一个任务命令
+  /// </summary>
+  public class JsonFileHelper
+  {
+    public const String DirPath = "./commandTasks";
+    public JsonFileHelper()
+    {
+
+    }
+
+    /// <summary>
+    /// 读取任务列表
+    /// </summary>
+    /// <returns></returns>
+    public async Task<List<TaskModel>> ReadAllAsync()
+    {
+
+      List<TaskModel> taskList = new List<TaskModel>();
+      if (Directory.Exists(DirPath))
+      {
+        DirectoryInfo dir = new DirectoryInfo(DirPath);
+        FileInfo[] files = dir.GetFiles();
+
+        foreach (FileInfo file in files)
+        {
+          StreamReader reader = file.OpenText();
+          String jsonString = await reader.ReadToEndAsync();
+          reader.Dispose();
+          TaskModel taskModel = JsonConvert.DeserializeObject<TaskModel>(jsonString);
+
+          taskList.Add(taskModel);
+        }
+
+      } else
+      {
+        Directory.CreateDirectory(DirPath);
+      }
+      return taskList;
+
+    }
+    /// <summary>
+    /// 读取某一任务配置
+    /// </summary>
+    /// <param name="fileName">任务名称</param>
+    /// <returns></returns>
+    public async Task<TaskModel> Read(String fileName)
+    {
+      TaskModel taskModel = new TaskModel();
+
+      FileInfo file = new FileInfo(Path.Combine(DirPath, fileName + ".json"));
+      if (!file.Exists)
+        return taskModel;
+      StreamReader reader = file.OpenText();
+      String jsonString = await reader.ReadToEndAsync();
+      reader.Dispose();
+
+      taskModel = JsonConvert.DeserializeObject<TaskModel>(jsonString);
+      return taskModel;
+    }
+
+    /// <summary>
+    /// 新建任务
+    /// </summary>
+    /// <param name="task">TaskModel</param>
+    /// <returns></returns>
+    public async Task<Boolean> Insert(TaskModel task)
+    {
+      FileInfo file = new FileInfo(Path.Combine(DirPath, task.Title + ".json"));
+      if (file.Exists)
+      {
+        return false;
+      }
+      StreamWriter writer = file.CreateText();
+      await writer.WriteAsync(JsonConvert.SerializeObject(task));
+      writer.Dispose();
+      return true;
+    }
+
+    /// <summary>
+    /// 删除一任务
+    /// </summary>
+    /// <param name="fileName">任务名称</param>
+    /// <returns></returns>
+    public Boolean Delete(String fileName)
+    {
+      FileInfo file = new FileInfo(Path.Combine(DirPath, fileName + ".json"));
+      if (file.Exists)
+      {
+        file.Delete();
+      }
+      return true;
+    }
+
+
+    public async Task<Boolean> Update(String fileName,TaskModel task)
+    {
+      Delete(fileName);
+      await Insert(task);
+      return true;
+    }
+  }
+}
---
+++ MSDev.Tools.CommandRunner/WebSocketRunner.cs
@@ -0,0 +1,83 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MSDev.Tools.CommandRunner
+{
+  /// <summary>
+  /// Use with Websocket
+  /// </summary>
+  public class WebSocketRunner
+  {
+    private readonly WebSocket _webSocket;
+    readonly Dictionary<String, String> _taskMap = new Dictionary<String, String>()
+    {
+      ["bingnews"] = "cd /var/task/queue;sudo dotnet MSDev.Taskschd.dll"
+    };
+
+    public WebSocketRunner(WebSocket webSocket)
+    {
+      _webSocket = webSocket;
+    }
+
+    public async Task Run(String command)
+    {
+      Process myProcess = new Process();
+
+      if (_taskMap.TryGetValue(command, out String value))
+      {
+        Console.WriteLine(value);
+        command = value;
+      }
+
+      Console.WriteLine("command is :" + command);
+
+      try
+      {
+        myProcess.StartInfo.UseShellExecute = false;
+        //linux
+        myProcess.StartInfo.FileName = "bash";
+        myProcess.StartInfo.Arguments = "-c \"" + command + "\"";
+
+        //windows
+        //myProcess.StartInfo.FileName = "powershell.exe";
+        //myProcess.StartInfo.Arguments = command;
+
+        Console.WriteLine("command is :"+command);
+
+        myProcess.StartInfo.CreateNoWindow = false;
+        myProcess.StartInfo.RedirectStandardOutput = true;
+        myProcess.StartInfo.StandardOutputEncoding = Encoding.UTF8;
+
+        myProcess.Start();
+
+        StreamReader reader = myProcess.StandardOutput;
+        String line = reader.ReadLine();
+
+        while (line != null)
+        {
+          await Echo(line);
+          Console.WriteLine(line);
+          line = reader.ReadLine();
+        }
+
+        myProcess.WaitForExit();
+        myProcess.Dispose();
+
+      } catch (Exception e)
+      {
+        Console.WriteLine(e.Message);
+      }
+    }
+    private async Task Echo(String message)
+    {
+      await _webSocket.SendAsync(
+        new ArraySegment<Byte>(Encoding.UTF8.GetBytes(message), 0, message.Length), WebSocketMessageType.Text, true, CancellationToken.None);
+    }
+  }
+}
